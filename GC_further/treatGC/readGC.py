87570e7eada1b03f91cf5453e815670dc95b7aac
neha rawal <nrawal@lxplus.cern.ch>
2022-07-08 10:58:46 +0200

    adding read file for the project

--- GC_further/treatGC/readGC.py
+++ GC_further/treatGC/readGC.py
@@ -59,12 +59,11 @@ for fn in fnames:
    print "running for file inside"
    for column in ("B", "C"):
        print("column : ", column)
-#      for fn in fnames:
    for column in("B"):
        print" calling Set Peak valley  function"
        gcs.setPeak(fn,column,name[column],ranges[column])
-#       print" calling get Peak valley  function not useful now"
-#       gcs.getPeakValley(fn,column,name[column])
+       # print" calling get Peak valley  function not useful now"
+       #gcs.getPeakValley(fn,column,name[column])
        print "calling Multi plot function now"
        gcs.plotMulti(fn,column)
        print "done with Multi plot function now"
@@ -79,16 +78,13 @@ print "function reference fnames_ref ", fnames_ref
 #for fn_ref in fnames_ref:
 print "reference files fn_ref  ", fn_ref
 print "running for ref file inside"
-#              for column in ("B","C"):
-#                print("column : ", column)
-#              for fn_ref in fnames_ref :
 
 for column in("B"):
     print" calling set Peak  function"
     gcs_ref.setPeak(fn_ref,column,name_ref[column],ranges_ref[column])
     print" calling get Peak valley  function"
-#    gcs_ref.getPeakValley(fn_ref,column,name_ref[column])
-#    print" calling multi plot function"
+    #gcs_ref.getPeakValley(fn_ref,column,name_ref[column])
+    print" calling multi plot function"
     gcs_ref.plotMulti(fn_ref,column)
     print" done with  multi plot function"
 
@@ -96,27 +92,32 @@ for column in("B"):
 #from here on the function that will be first called is stored in GC_plots
 gcs_plot = GC_plots.plots(fn, dir, verb, pdfname,fn_ref, dir, verb, pdfname)
 gcs_plot.setPeak(fn,fn_ref, "B", name["B"], name_ref["B"] ,ranges_ref["B"])
+                 
 #plot on the same canvas both original and reference files
 #gcs_plot.plot_org_ref(fn, "B", fn_ref, "B","first")
-
+            #
 # now shift the baselines and plot on same canvas
 # when you plot the function by just shifting to common baseline
 print"starting shfiting baseline"
+##gcs_plot.setPeak(fn,fn_ref, "B", name["B"], name_ref["B"] ,ranges_ref["B"])
+##gcs_plot.evaluate_integral(fn, fn_ref,"B")
 gcs_plot.shiftGC_baseline(fn, fn_ref, "B")
+gcs_plot.shiftGC_baseline_CO2(fn, fn_ref, "B")
 #gcs_plot.plot_org_ref(fn, "B", fn_ref, "B","second")
 
-# scale your Ar peak to fit the org
-# do not run setPeak again , just modify values only for reference not for original
+#scale your Ar peak to fit the org
+# ifnot run setPeak again , just modify values only for reference not for original
 gcs_plot.scaleGC_first_peak(fn, fn_ref, "B")
 gcs_plot.evaluate_integral(fn, fn_ref, "B")
 #gcs_plot.plot_org_ref(fn, "B", fn_ref, "B","third")
 
-# to shift along the x axis and then evaluate integral and plot on same plot
+## to shift along the x axis and then evaluate integral and plot on same plot
 gcs_plot.shiftGC_x(fn,fn_ref,"B")
-gcs_plot.setPeak_positions_ref(fn_ref,"B",ranges_ref["B"])
+gcs_plot.setPeak_positions_ref(fn_ref,"B",ranges_ref["B"], name_ref["B"])
 gcs_plot.evaluate_integral(fn, fn_ref, "B")
-gcs_plot.plot_org_ref(fn, "B", fn_ref, "B","fourth_new")
+gcs_plot.plot_org_ref(fn, "B", fn_ref, "B","fourth")
 
-# store values of peak and valleys for both reference and original plots
-#    for column in ("B","C"):
-#        gcs.plotDrift(column)
+
+ ## store values of peak and valleys for both reference and original plots
+ ##    for column in ("B","C"):
+ ##        gcs.plotDrift(column)
